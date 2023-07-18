6925a2230ff150fdd9abf2e4e3d66c26654573c6
Telma Kneeteman <tkneeteman@danaide.com.ar>
2023-07-18 20:44:49 -0300

    apikey

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
   BASE_URL: 'https://api.openai.com/v1/chat',
   MODEL: 'gpt-3.5-turbo',
-  API_KEY: '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',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
